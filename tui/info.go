17abcec3aef0ed001c13e224d1dcad0f2aecd1fe
Ajay <ajayds2001@gmail.com>
2024-04-12 23:52:42 -0400

    WIP

---
+++ tui/info.go
@@ -0,0 +1,15 @@
+package tui
+
+import "github.com/docker/docker/api/types/image"
+
+func PopulateImageInfoBox(image.Summary) {
+	panic("unimpl")
+}
+
+func PopulateVolumeInfoBox() {
+	panic("unimpl")
+}
+
+func PopulateContainerInfoBox() {
+	panic("unimpl")
+}
