ajayd-san/gomanagedocker

commit b7e47998d874bdc2dac86fe535b85f4009c134e2

fix: infobox styling

tui/styles.go
@@ -19,8 +19,8 @@ var (
 	moreInfoStyle = lipgloss.NewStyle().
 			Border(lipgloss.NormalBorder()).
 			BorderForeground(lipgloss.Color("69")).
-			Width(90).
-			Height(35)
+			Width(100).
+			MarginLeft(5).MarginTop(2)
 
 	infoEntryLabel = lipgloss.NewStyle().
 			Foreground(lipgloss.Color("49")).

tui/tabs.go
@@ -64,8 +64,10 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			Width(m.width - listDocStyle.GetHorizontalFrameSize() - 2).
 			Height(m.height - listDocStyle.GetVerticalFrameSize() - 3)
 
-			//change list dimentions when window size changes
-			// TODO: change width
+		moreInfoStyle = moreInfoStyle.Height(m.getActiveList().Height())
+
+		//change list dimentions when window size changes
+		// TODO: change width
 		for index := range m.TabContent {
 			m.getList(index).SetWidth(msg.Width)
 			m.getList(index).SetHeight(msg.Height - 7)
@@ -179,12 +181,12 @@ func (m Model) View() string {
 	list := m.TabContent[m.activeTab].View()
 	curItem := m.getSelectedItem()
 	infobox := PopulateInfoBox(tabId(m.activeTab), curItem)
+	infobox = moreInfoStyle.Render(infobox)
 
 	//TODO: align info box to right edge of the window
-	body_with_info := lipgloss.JoinHorizontal(lipgloss.Right, list, moreInfoStyle.Render(infobox))
-	// body_with_info = windowStyle.Render(body_with_info)
+	body_with_info := lipgloss.JoinHorizontal(lipgloss.Top, list, infobox)
+	body_with_info = windowStyle.Render(body_with_info)
 
-	// body := m.TabContent[m.activeTab].View()
 	doc.WriteString(row)
 	doc.WriteString("\n")
 

tui/list.go
@@ -32,14 +32,14 @@ func (m listModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m listModel) View() string {
-
 	return listDocStyle.Render(m.list.View())
 }
 
 func InitList(tab tabId) listModel {
 
 	items := make([]list.Item, 0)
-	m := listModel{list: list.New(items, list.NewDefaultDelegate(), 100, 30)}
+	m := listModel{list: list.New(items, list.NewDefaultDelegate(), 10, 30)}
+
 	m.list.SetShowTitle(false)
 
 	switch tab {

commit 7198b745d4304f42caf357868f35068df55bc576

fix: ui breakage when filtering, bug introduced in b7e4799

tui/styles.go
@@ -19,8 +19,8 @@ var (
 	moreInfoStyle = lipgloss.NewStyle().
 			Border(lipgloss.NormalBorder()).
 			BorderForeground(lipgloss.Color("69")).
-			Width(100).
-			MarginLeft(5).MarginTop(2)
+			Width(90).
+			Height(30).MarginTop(2).MarginLeft(5)
 
 	infoEntryLabel = lipgloss.NewStyle().
 			Foreground(lipgloss.Color("49")).

tui/tabs.go
@@ -63,8 +63,6 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			Width(m.width - listDocStyle.GetHorizontalFrameSize() - 2).
 			Height(m.height - listDocStyle.GetVerticalFrameSize() - 3)
 
-		moreInfoStyle = moreInfoStyle.Height(m.getActiveList().Height())
-
 		//change list dimentions when window size changes
 		// TODO: change width
 		for index := range m.TabContent {
@@ -195,7 +193,7 @@ func (m Model) View() string {
 
 	//TODO: align info box to right edge of the window
 	body_with_info := lipgloss.JoinHorizontal(lipgloss.Top, list, infobox)
-	body_with_info = windowStyle.Render(body_with_info)
+	// body_with_info = windowStyle.Render(body_with_info)
 
 	doc.WriteString(row)
 	doc.WriteString("\n")