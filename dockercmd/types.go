7426e0b6806302cd519e5b54e5adfc13d6a5bcd0
Ajay <ajayds2001@gmail.com>
2024-06-11 21:18:37 +0530

    add: impl docker scout command

--- dockercmd/types.go
+++ dockercmd/types.go
@@ -7,6 +7,35 @@ import (
 	"github.com/docker/docker/client"
 )
 
+type ImageVulnerabilities struct {
+	Label           string
+	ImageName       string
+	Critical        string
+	High            string
+	Medium          string
+	Low             string
+	UnknownSeverity string
+}
+
+// takes [][]bytes returned by regex.FindSubmatches and returns ImageVulnerabilities
+func makeImageVulnerabilities(submatches [][]byte) ImageVulnerabilities {
+	return ImageVulnerabilities{
+		Label:           string(submatches[1]),
+		ImageName:       string(submatches[2]),
+		Critical:        string(submatches[3]),
+		High:            string(submatches[4]),
+		Medium:          string(submatches[5]),
+		Low:             string(submatches[6]),
+		UnknownSeverity: string(submatches[8]),
+	}
+
+}
+
+type ScoutData struct {
+	ImageVulEntries []ImageVulnerabilities
+	TargetDigest    string
+}
+
 type DockerClient struct {
 	cli               *client.Client
 	containerListArgs container.ListOptions
