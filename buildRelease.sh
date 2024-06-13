8f8d8704b63e86958fb35bf02b8b3b1247e9df76
Ajay <ajayds2001@gmail.com>
2024-06-13 16:48:54 +0530

    mod: build script now builds tar balls

--- buildRelease.sh
+++ buildRelease.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-RELEASE=$1
+shopt -s extglob
 
+RELEASE=$1
 if [[ -z $1 ]];
 then
     echo "No release version passed"
@@ -11,16 +12,28 @@ else
 fi 
 
 if [ -d ./releases/ ]; then
-    rm -f ./releases/*
+    rm -fr ./releases/*
 fi 
 
 #linux
-env GOOS=linux GOARCH=amd64 go build -o ./releases/"gomanagedocker_linux_$RELEASE" github.com/ajayd-san/gomanagedocker
+env GOOS=linux GOARCH=amd64 go build -o ./releases/linux_amd64_$RELEASE/"gmd" github.com/ajayd-san/gomanagedocker
+
 
 # macos
-env GOOS=darwin GOARCH=amd64 go build -o ./releases/"gomanagedocker_darwin64_$RELEASE" github.com/ajayd-san/gomanagedocker
-env GOOS=darwin GOARCH=arm64 go build -o ./releases/"gomanagedocker_darwin_arm64_$RELEASE" github.com/ajayd-san/gomanagedocker
+env GOOS=darwin GOARCH=amd64 go build -o ./releases/darwin_amd64_$RELEASE/"gmd" github.com/ajayd-san/gomanagedocker
+env GOOS=darwin GOARCH=arm64 go build -o ./releases/darwin_arm64_$RELEASE/"gmd" github.com/ajayd-san/gomanagedocker
+
 
 #windows
-env GOOS=windows GOARCH=amd64 go build -o ./releases/"gomanagedocker_windows64_$RELEASE.exe" github.com/ajayd-san/gomanagedocker
+env GOOS=windows GOARCH=amd64 go build -o ./releases/windows_amd64_$RELEASE/"gmd.exe" github.com/ajayd-san/gomanagedocker
 
+cd releases
+
+## make a tar ball to save space
+tar czf gomanagedocker_linux_amd64_$RELEASE.tar.gz linux_amd64_$RELEASE
+tar czf gomanagedocker_darwin_amd64_$RELEASE.tar.gz darwin_amd64_$RELEASE
+tar czf gomanagedocker_darwin_arm64_$RELEASE.tar.gz darwin_arm64_$RELEASE
+tar czf gomanagedocker_windows_amd64_$RELEASE.tar.gz windows_amd64_$RELEASE
+
+## remove all files in releases that are not tar balls
+rm -r !(*.tar.gz)
