1f565ff2854388f7c3cd18033e04d088ee55dc3c
Ajay <ajayds2001@gmail.com>
2024-06-28 22:05:11 +0530

    add: tests for container

--- dockercmd/container_test.go
+++ dockercmd/container_test.go
@@ -2,10 +2,18 @@ package dockercmd
 
 import (
 	"testing"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"gotest.tools/v3/assert"
 )
 
 var dockerclient = NewDockerClient()
 
+/*
+was useful in deciding if i should query the containersize by default
+spoiler: querying container size has huge performance impact something like +5000% time increase or something
+*/
 func BenchmarkContainerList(b *testing.B) {
 	b.Run("Showing container size", func(b *testing.B) {
 		for range b.N {
@@ -17,4 +25,301 @@ func BenchmarkContainerList(b *testing.B) {
 			dockerclient.ListContainers(true)
 		}
 	})
+}
+
+func TestListContainer(t *testing.T) {
+
+	containers := []types.Container{
+		{
+			ID:         "1",
+			SizeRw:     200,
+			SizeRootFs: 400,
+			State:      "running",
+			Status:     "",
+		},
+		{
+			ID:         "2",
+			SizeRw:     201,
+			SizeRootFs: 401,
+			State:      "running",
+		},
+		{
+			ID:         "3",
+			SizeRw:     202,
+			SizeRootFs: 402,
+			State:      "created",
+		},
+		{
+			ID:         "4",
+			SizeRw:     203,
+			SizeRootFs: 403,
+			State:      "stopped",
+		},
+	}
+
+	t.Run("Default (not showing all containers)", func(t *testing.T) {
+
+		dclient := DockerClient{
+			cli: &MockApi{
+				mockContainers:  containers,
+				CommonAPIClient: nil,
+			},
+			containerListArgs: container.ListOptions{},
+		}
+
+		got := dclient.ListContainers(false)
+
+		want := []types.Container{
+			{
+				ID:         "1",
+				SizeRw:     -1,
+				SizeRootFs: -1,
+				State:      "running",
+			},
+			{
+				ID:         "2",
+				SizeRw:     -1,
+				SizeRootFs: -1,
+				State:      "running",
+			},
+		}
+
+		assert.DeepEqual(t, want, got)
+	})
+
+	t.Run("Showing all containers", func(t *testing.T) {
+
+		dclient := DockerClient{
+			cli: &MockApi{
+				mockContainers:  containers,
+				CommonAPIClient: nil,
+			},
+			containerListArgs: container.ListOptions{},
+		}
+
+		dclient.ToggleContainerListAll()
+
+		got := dclient.ListContainers(false)
+
+		want := []types.Container{
+			{
+				ID:         "1",
+				SizeRw:     -1,
+				SizeRootFs: -1,
+				State:      "running",
+				Status:     "",
+			},
+			{
+				ID:         "2",
+				SizeRw:     -1,
+				SizeRootFs: -1,
+				State:      "running",
+			},
+			{
+				ID:         "3",
+				SizeRw:     -1,
+				SizeRootFs: -1,
+				State:      "created",
+			},
+			{
+				ID:         "4",
+				SizeRw:     -1,
+				SizeRootFs: -1,
+				State:      "stopped",
+			},
+		}
+
+		assert.DeepEqual(t, got, want)
+	})
+
+	t.Run("Also calculate sizes", func(t *testing.T) {
+
+		dclient := DockerClient{
+			cli: &MockApi{
+				mockContainers:  containers,
+				CommonAPIClient: nil,
+			},
+			containerListArgs: container.ListOptions{},
+		}
+
+		dclient.ToggleContainerListAll()
+
+		got := dclient.ListContainers(true)
+		want := containers
+
+		assert.DeepEqual(t, got, want)
+	})
+}
+
+func TestContainerToggleListAll(t *testing.T) {
+	dclient := DockerClient{
+		cli: &MockApi{
+			mockContainers:  nil,
+			CommonAPIClient: nil,
+		},
+		containerListArgs: container.ListOptions{},
+	}
+
+	assert.Assert(t, !dclient.containerListArgs.All)
+	dclient.ToggleContainerListAll()
+	assert.Assert(t, dclient.containerListArgs.All)
+}
+
+func TestToggleStartStopContainer(t *testing.T) {
+	containers := []types.Container{
+		{
+			ID:         "1",
+			SizeRw:     200,
+			SizeRootFs: 400,
+			State:      "running",
+			Status:     "",
+		},
+		{
+			ID:         "2",
+			SizeRw:     201,
+			SizeRootFs: 401,
+			State:      "running",
+		},
+	}
+
+	dclient := DockerClient{
+		cli: &MockApi{
+			mockContainers:  containers,
+			CommonAPIClient: nil,
+		},
+		containerListArgs: container.ListOptions{},
+	}
+
+	t.Run("Stopping container test", func(t *testing.T) {
+		dclient.ToggleStartStopContainer("2")
+
+		state := dclient.cli.(*MockApi).mockContainers
+
+		assert.Assert(t, state[1].State == "stopped")
+	})
+
+	t.Run("Start container test", func(t *testing.T) {
+		dclient.ToggleStartStopContainer("2")
+
+		state := dclient.cli.(*MockApi).mockContainers
+
+		assert.Assert(t, state[1].State == "running")
+	})
+}
+
+func TestPauseUnpauseContainer(t *testing.T) {
+	containers := []types.Container{
+		{
+			ID:    "1",
+			State: "running",
+		},
+		{
+			ID:    "2",
+			State: "stopped",
+		},
+	}
+
+	dclient := DockerClient{
+		cli: &MockApi{
+			mockContainers:  containers,
+			CommonAPIClient: nil,
+		},
+		containerListArgs: container.ListOptions{},
+	}
+
+	t.Run("Pause running container", func(t *testing.T) {
+		id := "1"
+		err := dclient.TogglePauseResume(id)
+		assert.NilError(t, err)
+		containers := dclient.cli.(*MockApi).mockContainers
+
+		assert.Assert(t, containers[0].State == "paused")
+	})
+
+	t.Run("unpause running container", func(t *testing.T) {
+		id := "1"
+
+		err := dclient.TogglePauseResume(id)
+		assert.NilError(t, err)
+
+		containers := dclient.cli.(*MockApi).mockContainers
+
+		assert.Assert(t, containers[0].State == "running")
+	})
+
+	t.Run("unpause stopped container(should throw error)", func(t *testing.T) {
+		id := "2"
+		err := dclient.TogglePauseResume(id)
+		assert.ErrorContains(t, err, "Cannot Pause/unPause a")
+	})
+}
+
+func TestDeleteContainer(t *testing.T) {
+	containers := []types.Container{
+		{
+			ID:    "1",
+			State: "running",
+		},
+		{
+			ID:    "2",
+			State: "stopped",
+		},
+	}
+
+	dclient := DockerClient{
+		cli: &MockApi{
+			mockContainers:  containers,
+			CommonAPIClient: nil,
+		},
+		containerListArgs: container.ListOptions{},
+	}
+
+	t.Run("Delete stopped container", func(t *testing.T) {
+		id := "2"
+		err := dclient.DeleteContainer(id, container.RemoveOptions{})
+		assert.NilError(t, err)
+	})
+
+	t.Run("Try delete runing container(fails)", func(t *testing.T) {
+		id := "1"
+		err := dclient.DeleteContainer(id, container.RemoveOptions{})
+		assert.ErrorContains(t, err, "container is running")
+	})
+}
+
+func TestPruneContainer(t *testing.T) {
+	containers := []types.Container{
+		{
+			ID:    "1",
+			State: "stopped",
+		},
+		{
+			ID:    "2",
+			State: "running",
+		},
+		{
+			ID:    "3",
+			State: "stopped",
+		},
+	}
+
+	dclient := DockerClient{
+		cli: &MockApi{
+			mockContainers:  containers,
+			CommonAPIClient: nil,
+		},
+		containerListArgs: container.ListOptions{},
+	}
+	dclient.PruneContainers()
+
+	want := []types.Container{
+		{
+			ID:    "2",
+			State: "running",
+		},
+	}
+
+	got := dclient.cli.(*MockApi).mockContainers
+
+	assert.DeepEqual(t, want, got)
 }
--- dockercmd/test_utils.go
+++ dockercmd/test_utils.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"io"
 	"slices"
 
 	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	dimage "github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/api/types/volume"
@@ -14,9 +16,156 @@ import (
 )
 
 type MockApi struct {
-	mockVolumes []*volume.Volume
-	mockImages  []dimage.Summary
+	mockContainers []types.Container
+	mockVolumes    []*volume.Volume
+	mockImages     []dimage.Summary
 	client.CommonAPIClient
+}
+
+func (mo *MockApi) ContainerInspect(ctx context.Context, container string) (types.ContainerJSON, error) {
+	index := slices.IndexFunc(mo.mockContainers, func(cont types.Container) bool {
+		if cont.ID == container {
+			return true
+		}
+
+		return false
+	})
+
+	cur := mo.mockContainers[index]
+
+	state := types.ContainerState{}
+	if cur.State == "running" {
+		state.Running = true
+	} else if cur.State == "paused" {
+		state.Paused = true
+	}
+
+	return types.ContainerJSON{
+		ContainerJSONBase: &types.ContainerJSONBase{
+			ID:    cur.ID,
+			State: &state,
+		},
+	}, nil
+
+}
+
+func (m *MockApi) ContainerList(ctx context.Context, options container.ListOptions) ([]types.Container, error) {
+	final := []types.Container{}
+
+	for _, cont := range m.mockContainers {
+		if cont.State == "running" || options.All {
+			if !options.Size {
+				cont.SizeRw = -1
+				cont.SizeRootFs = -1
+			}
+
+			final = append(final, cont)
+		}
+	}
+
+	return final, nil
+}
+
+func (mo *MockApi) ContainerLogs(ctx context.Context, container string, options container.LogsOptions) (io.ReadCloser, error) {
+	panic("not implemented") // TODO: Implement
+}
+
+func (mo *MockApi) ContainerPause(ctx context.Context, container string) error {
+
+	index := slices.IndexFunc(mo.mockContainers, func(cont types.Container) bool {
+		if cont.ID == container {
+			return true
+		}
+
+		return false
+	})
+
+	mo.mockContainers[index].State = "paused"
+
+	return nil
+}
+
+func (mo *MockApi) ContainerRemove(ctx context.Context, container string, options container.RemoveOptions) error {
+
+	index := slices.IndexFunc(mo.mockContainers, func(cont types.Container) bool {
+		if cont.ID == container {
+			return true
+		}
+
+		return false
+	})
+
+	if mo.mockContainers[index].State == "running" {
+		//not exact error but works for now
+		return errors.New("container is running")
+	}
+
+	mo.mockContainers = slices.Delete(mo.mockContainers, index, index+1)
+
+	return nil
+}
+
+func (mo *MockApi) ContainerRestart(ctx context.Context, container string, options container.StopOptions) error {
+	panic("not implemented") // TODO: Implement
+}
+func (mo *MockApi) ContainerStart(ctx context.Context, container string, options container.StartOptions) error {
+	index := slices.IndexFunc(mo.mockContainers, func(cont types.Container) bool {
+		if cont.ID == container {
+			return true
+		}
+
+		return false
+	})
+
+	mo.mockContainers[index].State = "running"
+
+	return nil
+}
+
+func (mo *MockApi) ContainerStop(ctx context.Context, container string, options container.StopOptions) error {
+	index := slices.IndexFunc(mo.mockContainers, func(cont types.Container) bool {
+		if cont.ID == container {
+			return true
+		}
+
+		return false
+	})
+
+	mo.mockContainers[index].State = "stopped"
+
+	return nil
+}
+
+func (mo *MockApi) ContainerUnpause(ctx context.Context, container string) error {
+
+	index := slices.IndexFunc(mo.mockContainers, func(cont types.Container) bool {
+		if cont.ID == container {
+			return true
+		}
+
+		return false
+	})
+
+	mo.mockContainers[index].State = "running"
+
+	return nil
+}
+
+func (mo *MockApi) ContainersPrune(ctx context.Context, pruneFilters filters.Args) (types.ContainersPruneReport, error) {
+
+	final := []types.Container{}
+
+	for _, cont := range mo.mockContainers {
+		if cont.State == "stopped" {
+			continue
+		}
+
+		final = append(final, cont)
+	}
+
+	mo.mockContainers = final
+	return types.ContainersPruneReport{}, nil
+
 }
 
 func (m *MockApi) VolumeList(ctx context.Context, options volume.ListOptions) (volume.ListResponse, error) {
