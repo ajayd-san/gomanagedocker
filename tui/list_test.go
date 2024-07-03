e329703814d93cdc24fa59da6f9234605cd23c1b
Ajay <ajayds2001@gmail.com>
2024-07-03 11:47:58 +0530

    tests: added tests for list.go

---
+++ tui/list_test.go
@@ -0,0 +1,213 @@
+package tui
+
+import (
+	"testing"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/image"
+	"gotest.tools/v3/assert"
+)
+
+func TestUpdateExistingIds(t *testing.T) {
+
+	containers := []types.Container{
+		{
+			Names:      []string{"a"},
+			ID:         "1",
+			SizeRw:     1e+9,
+			SizeRootFs: 2e+9,
+			State:      "running",
+			Status:     "",
+		},
+		{
+			Names:      []string{"b"},
+			ID:         "2",
+			SizeRw:     201,
+			SizeRootFs: 401,
+			State:      "running",
+		},
+		{
+			Names:      []string{"c"},
+			ID:         "3",
+			SizeRw:     202,
+			SizeRootFs: 402,
+			State:      "running",
+		},
+		{
+
+			Names:      []string{"d"},
+			ID:         "4",
+			SizeRw:     203,
+			SizeRootFs: 403,
+			State:      "running",
+		},
+	}
+
+	imgs := []image.Summary{
+		{
+			Containers: 0,
+			ID:         "0",
+			RepoTags:   []string{"a"},
+		},
+
+		{
+			Containers: 0,
+			ID:         "1",
+			RepoTags:   []string{"b"},
+		},
+		{
+			Containers: 3,
+			ID:         "2",
+			RepoTags:   []string{"c"},
+		},
+		{
+			Containers: 0,
+			ID:         "3",
+			RepoTags:   []string{"d"},
+		},
+	}
+	CONTAINERS = 0
+	IMAGES = 1
+
+	t.Run("Assert  images Ids", func(t *testing.T) {
+		contList := InitList(0)
+		dres := makeContainerItems(containers)
+		contList.updateExistigIds(&dres)
+		want := map[string]struct{}{
+			"1": {},
+			"2": {},
+			"3": {},
+			"4": {},
+		}
+
+		assert.DeepEqual(t, contList.ExistingIds, want)
+	})
+
+	t.Run("Assert  image Ids", func(t *testing.T) {
+		imgsList := InitList(IMAGES)
+		dres := makeImageItems(imgs)
+		imgsList.updateExistigIds(&dres)
+		want := map[string]struct{}{
+			"0": {},
+			"1": {},
+			"2": {},
+			"3": {},
+		}
+		assert.DeepEqual(t, imgsList.ExistingIds, want)
+	})
+}
+
+func TestUpdateTab(t *testing.T) {
+	IMAGES = 0
+	CONTAINERS = 1
+
+	imgs := []image.Summary{
+		{
+			Containers: 0,
+			ID:         "0",
+			RepoTags:   []string{"a"},
+		},
+
+		{
+			Containers: 0,
+			ID:         "1",
+			RepoTags:   []string{"b"},
+		},
+		{
+			Containers: 3,
+			ID:         "2",
+			RepoTags:   []string{"c"},
+		},
+		{
+			Containers: 0,
+			ID:         "3",
+			RepoTags:   []string{"d"},
+		},
+	}
+
+	list := InitList(IMAGES)
+	t.Run("Assert Images subset", func(t *testing.T) {
+		subset := imgs[:2]
+		dres := makeImageItems(subset)
+		list.updateTab(dres)
+
+		liItems := list.list.Items()
+
+		for i := range len(liItems) {
+			got := liItems[i].(imageItem)
+			want := subset[i]
+
+			assert.DeepEqual(t, got.Summary, want)
+		}
+	})
+
+	t.Run("Assert Images full", func(t *testing.T) {
+		dres := makeImageItems(imgs)
+		list.updateTab(dres)
+
+		liItems := list.list.Items()
+
+		for i := range len(liItems) {
+			got := liItems[i].(imageItem)
+			want := imgs[i]
+
+			assert.DeepEqual(t, got.Summary, want)
+		}
+	})
+
+}
+
+func TestUpdate(t *testing.T) {
+	IMAGES = 0
+	CONTAINERS = 1
+
+	imgs := []image.Summary{
+		{
+			Containers: 0,
+			ID:         "0",
+			RepoTags:   []string{"a"},
+		},
+
+		{
+			Containers: 0,
+			ID:         "1",
+			RepoTags:   []string{"b"},
+		},
+		{
+			Containers: 3,
+			ID:         "2",
+			RepoTags:   []string{"c"},
+		},
+		{
+			Containers: 0,
+			ID:         "3",
+			RepoTags:   []string{"d"},
+		},
+	}
+
+	imgList := InitList(IMAGES)
+
+	t.Run("Update images", func(t *testing.T) {
+		dres := makeImageItems(imgs)
+		temp, _ := imgList.Update(dres)
+		imgList = temp.(listModel)
+
+		listItems := imgList.list.Items()
+
+		for i := range len(listItems) {
+			got := listItems[i].(imageItem)
+			want := imgs[i]
+
+			assert.DeepEqual(t, got.Summary, want)
+		}
+	})
+
+	t.Run("Update list size", func(t *testing.T) {
+		assert.Equal(t, imgList.list.Width(), 60)
+		temp, _ := imgList.Update(tea.WindowSizeMsg{Width: 210, Height: 100})
+		imgList := temp.(listModel)
+
+		assert.Equal(t, imgList.list.Width(), int(210*0.3))
+	})
+}
