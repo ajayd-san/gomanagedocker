54f32e9bdf1f7a9cf150b1aa7fa3ddbc1a49fe33
Ajay <ajayds2001@gmail.com>
2024-10-24 22:35:38 -0400

    add: impl wrapper around podman image api

---
+++ podman/images.go
@@ -0,0 +1,24 @@
+package podman
+
+import (
+	"github.com/containers/podman/v5/pkg/bindings/images"
+	"github.com/containers/podman/v5/pkg/domain/entities/reports"
+	"github.com/containers/podman/v5/pkg/domain/entities/types"
+	tf "github.com/containers/podman/v5/pkg/domain/entities/types"
+)
+
+func (p *PodmanClient) ImageList(opts *images.ListOptions) ([]*tf.ImageSummary, error) {
+	return images.List(p.ctx, opts)
+}
+
+func (pc *PodmanClient) Remove(image_ids []string, opts *images.RemoveOptions) (*tf.ImageRemoveReport, []error) {
+	return images.Remove(pc.ctx, image_ids, opts)
+}
+
+func (pc *PodmanClient) Prune(opts *images.PruneOptions) ([]*reports.PruneReport, error) {
+	return images.Prune(pc.ctx, opts)
+}
+
+func (p *PodmanClient) ImageBuild(containerFiles []string, opts types.BuildOptions) (*tf.BuildReport, error) {
+	return images.Build(p.ctx, containerFiles, opts)
+}
